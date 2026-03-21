363d2c69632642692b41796c856dff40ac394089
Jesús Marcos <marcosjesusm@gmail.com>
2026-03-21 17:53:58 +0100

    fix: add data.json volume mount + restore data + fix branch in actualiza.sh

--- actualiza.sh
+++ actualiza.sh
@@ -2,7 +2,7 @@
 cd /home/jesus/docker/volumes/agent-coordinator
 git fetch origin
 git reset --hard origin/main
-git pull origin main
+git pull origin master
 bash compila.sh
 docker compose down
 docker compose up -d --build
